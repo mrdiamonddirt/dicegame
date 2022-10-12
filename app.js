82222e8a5f0eb625c6f5de1855f8d1491994f846
RowDog <mrdiamonddirt@gmail.com>
2022-10-12 18:29:35 +0100

    added lose screen

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const resumePlay = document.getElementById('resume');
 const rollDie = document.getElementById('rollDie');
 const previousDi = document.getElementById('previous');
 const win = document.getElementById('win');
+const lose = document.getElementById('lose');
 var diceRoll = Math.floor( Math.random() * 6 ) +1;
 var playingGame = false;
 var currentScore = 0;
 var lastdie = "";
+var won = false;
 
 
 // start game event
@@ -44,19 +46,27 @@ startplay1.addEventListener("click", function () {
   });
 
   // Die Rolling Event
-
   rollDie.addEventListener('click', function() {
     var diceRoll = Math.floor( Math.random() * 6 ) +1;
     // console.log(`Starting Score ${currentScore}`)
     currentScore = currentScore + diceRoll;
     if (diceRoll == 1){
-      console.log('Event Lose')
-      return;
-    };
-    if (currentScore >= 20){
+      console.log('Event Lose');
+      lose.style.display = 'flex';
+      var losebtn = document.getElementById('winbtn');
+      losebtn.addEventListener('click', function() {
+      lose.style.display = 'none';
+          } ) };
+      if (currentScore >= 20) {
+      won = true;
       console.log('Event Win')
       win.style.display = 'flex';
-    };
+      var winbtn = document.getElementById('winbtn')
+      winbtn.addEventListener('click', function() {
+      win.style.display = 'none';
+        
+      })
+    }
     // console.log(`Dice Roll ${diceRoll}`)
     // console.log(`Current Score ${currentScore}`)
     document.getElementById('number').innerHTML = diceRoll;
