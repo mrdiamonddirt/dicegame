5339acfa374509eb13c48c9f7cb68ca6c2ef8768
RowDog <mrdiamonddirt@gmail.com>
2022-10-12 13:33:47 +0100

    menu and counter for single player

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const menubutton = document.getElementById('menubutton');
 const resumePlay = document.getElementById('resume');
 const rollDie = document.getElementById('rollDie');
 var playingGame = false;
+var currentScore = 0;
 
 startplay1.addEventListener("click", function () {
     console.log("clicked 1")
@@ -28,6 +29,10 @@ startplay1.addEventListener("click", function () {
 
   rollDie.addEventListener('click', function() {
     var diceRoll = Math.floor( Math.random() * 6 ) +1;
+    console.log(`Starting Score ${currentScore}`)
+    currentScore = currentScore + diceRoll;
+    console.log(`Dice Roll ${diceRoll}`)
+    console.log(`Current Score ${currentScore}`)
     document.getElementById('number').innerHTML = diceRoll;
-    console.log(diceRoll)
+    document.getElementById('score').innerHTML = currentScore;
   })
