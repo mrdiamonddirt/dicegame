93ed1e2a858287d2136479f5dbd72b86ac020530
RowDog <mrdiamonddirt@gmail.com>
2022-10-12 13:13:58 +0100

    menu and functioning random numbers

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const startplay1 = document.getElementById('onePstart');
 const startplay2 = document.getElementById('twoPstart');
-
+const menubutton = document.getElementById('menubutton');
+const resumePlay = document.getElementById('resume');
+const rollDie = document.getElementById('rollDie');
+var playingGame = false;
 
 startplay1.addEventListener("click", function () {
     console.log("clicked 1")
     document.getElementById('startgame').style.display = 'none';
+    playingGame = true;
+    console.log(playingGame)
   });
 
   startplay2.addEventListener("click", function () {
     console.log("clicked 2")
   });
+
+  menubutton.addEventListener('click', function() {
+    document.getElementById('startgame').style.display = 'flex';
+    if (playingGame == true) {
+        document.getElementById('resume').style.display = 'block';
+        resumePlay.addEventListener('click', function() {
+        document.getElementById('startgame').style.display = 'none';    
+        })
+    }
+  });
+
+  rollDie.addEventListener('click', function() {
+    var diceRoll = Math.floor( Math.random() * 6 ) +1;
+    document.getElementById('number').innerHTML = diceRoll;
+    console.log(diceRoll)
+  })
