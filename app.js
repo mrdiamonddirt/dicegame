514cb3424e5088e1482976bcdf95a45b66998f5b
RowDog <mrdiamonddirt@gmail.com>
2022-10-12 11:41:15 +0100

    display menu hide on click

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const startplay2 = document.getElementById('twoPstart');
 
 startplay1.addEventListener("click", function () {
     console.log("clicked 1")
+    document.getElementById('startgame').style.display = 'none';
   });
 
   startplay2.addEventListener("click", function () {
