bb8c08acfed81aa565b5e0483a5e7a826c03d0a2
RowDog <mrdiamonddirt@gmail.com>
2022-10-12 11:39:12 +0100

    setup basic menu buttons

---
+++ app.js
@@ -0,0 +1,11 @@
+const startplay1 = document.getElementById('onePstart');
+const startplay2 = document.getElementById('twoPstart');
+
+
+startplay1.addEventListener("click", function () {
+    console.log("clicked 1")
+  });
+
+  startplay2.addEventListener("click", function () {
+    console.log("clicked 2")
+  });
