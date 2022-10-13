3b635f90d4812644a4eb6b35b9a9f09913a4fbd8
RowDog <mrdiamonddirt@gmail.com>
2022-10-13 07:09:10 +0100

    player selection

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const menubutton = document.getElementById("menubutton");
 const resumePlay = document.getElementById("resume");
 const rollDie = document.getElementById("rollDie");
 const previousDi = document.getElementById("previous");
+const previousDi2 = document.getElementById("previous2");
 const win = document.getElementById("win");
 const lose = document.getElementById("lose");
 var diceRoll = Math.floor(Math.random() * 6) + 1;
+var diceRoll2 = Math.floor(Math.random() * 6) + 1;
 var playingGame = false;
-var currentScore = 0;
+var currentScorep1 = 0;
 var lastdie = "";
 var won = false;
+var player = 0;
 
 // start game event
 startplay1.addEventListener("click", function () {
@@ -22,17 +25,18 @@ startplay1.addEventListener("click", function () {
   showgame1.style.display = 'flex';
   playingGame = true;
   console.log(playingGame);
-  //make sure button is there
+  //make sure roll button is there
   document.getElementById('rollDie').style.display = 'inline';
   // reset score
-  currentScore = 0;
+  currentScorep1 = 0;
   diceRoll = 0;
   console.log(`Dice Roll ${diceRoll}`);
-  console.log(`Current Score ${currentScore}`);
+  console.log(`Current Score ${currentScorep1}`);
   document.getElementById("number").innerHTML = diceRoll;
-  document.getElementById("score").innerHTML = currentScore;
+  document.getElementById("score").innerHTML = currentScorep1;
   //delete history
   previousDi.innerHTML = "";
+  previousDi2.innerHTML = "";
   console.log(`previous di ${previousDi.innerHTML}`);
 });
 
@@ -41,6 +45,18 @@ startplay2.addEventListener("click", function () {
   document.getElementById("startgame").style.display = "none";
 game2.style.display = 'flex';
 game1.style.display = 'none';
+//make sure roll button is there
+document.getElementById('rollDie2').style.display = 'inline';
+//clear old scores
+currentScorep1 = 0;
+diceRoll = 0;
+console.log(`Dice Roll ${diceRoll}`);
+console.log(`Current Score ${currentScorep1}`);
+document.getElementById("number").innerHTML = diceRoll;
+document.getElementById("score").innerHTML = currentScorep1;
+//delete history
+previousDi.innerHTML = "";
+console.log(`previous di ${previousDi.innerHTML}`);
 });
 
 menubutton.addEventListener("click", function () {
@@ -53,11 +69,11 @@ menubutton.addEventListener("click", function () {
   }
 });
 
-// Die Rolling Event
+// Die Rolling Event game1
 rollDie.addEventListener("click", function () {
   var diceRoll = Math.floor(Math.random() * 6) + 1;
-  // console.log(`Starting Score ${currentScore}`)
-  currentScore = currentScore + diceRoll;
+  // console.log(`Starting Score ${currentScorep1}`)
+  currentScorep1 = currentScorep1 + diceRoll;
   //lose event
   if (diceRoll == 1) {
     console.log("Event Lose");
@@ -75,7 +91,7 @@ rollDie.addEventListener("click", function () {
       // reset history
       previousDi.innerHTML = "";
       // reset highscore
-      currentScore = 0;
+      currentScorep1 = 0;
       document.getElementById("score").innerHTML = 0;
     });
     // hide lose screen
@@ -85,7 +101,7 @@ rollDie.addEventListener("click", function () {
     })
 
   }
-  if (currentScore >= 20 & won == false) {
+  if (currentScorep1 >= 20 & won == false) {
     won = true;
     console.log("Event Win");
     win.style.display = "flex";
@@ -97,9 +113,86 @@ rollDie.addEventListener("click", function () {
     });
   }
   // console.log(`Dice Roll ${diceRoll}`)
-  // console.log(`Current Score ${currentScore}`)
+  // console.log(`Current Score ${currentScorep1}`)
   document.getElementById("number").innerHTML = diceRoll;
-  document.getElementById("score").innerHTML = currentScore;
+  document.getElementById("score").innerHTML = currentScorep1;
   previousDi.innerHTML = previousDi.innerHTML + diceRoll;
   console.log(`previous di ${previousDi.innerHTML}`);
 });
+
+// Die Rolling Event for game 2
+rollDie2.addEventListener("click", function () {
+  if (player == 0){
+    console.log('select player')
+    alert('no player selected');
+    return;
+  };
+  if (player == 1) {
+    console.log('player 1 selected')
+  };
+  if (player == 2) {
+    console.log('player 2 selected')
+  };
+  var diceRoll2 = Math.floor(Math.random() * 6) + 1;
+  console.log(`dice roll game 2 ${currentScorep1}`)
+  //lose event
+  currentScorep1 = currentScorep1 + diceRoll2;
+  //lose event
+  if (diceRoll2 == 1) {
+    console.log("Event Lose");
+    win.style.display = "none";
+    lose.style.display = "flex";
+    //remove roll button
+    document.getElementById('rollDie2').style.display = 'none';
+    var losebtn = document.getElementById("losebtn");
+    // listen for button click on lose screen
+    losebtn.addEventListener("click", function () {
+      //remove object
+      lose.style.display = "none";
+      //bring button back
+      document.getElementById('rollDie').style.display = 'inline';
+      document.getElementById('rollDie2').style.display = 'inline';
+      // reset history
+      previousDi2.innerHTML = "";
+      // reset highscore
+      currentScorep1 = 0;
+      document.getElementById("score2").innerHTML = 0;
+    });
+    // hide lose screen
+    var hidelose = document.getElementById("hidelose");
+    hidelose.addEventListener('click', function(){
+      lose.style.display = "none";
+    })
+
+  }
+  if (currentScorep1 >= 20 & won == false) {
+    won = true;
+    console.log("Event Win");
+    win.style.display = "flex";
+    var winbtn = document.getElementById("winbtn");
+    // listen for button click on win screen
+    winbtn.addEventListener("click", function () {
+      win.style.display = "none";
+      
+    });
+  }
+  // console.log(`Dice Roll ${diceRoll}`)
+  // console.log(`Current Score ${currentScorep1}`)
+  document.getElementById("number2").innerHTML = diceRoll2;
+  document.getElementById("score2").innerHTML = currentScorep1;
+  previousDi2.innerHTML = previousDi2.innerHTML + diceRoll2;
+  console.log(`previous di ${previousDi2.innerHTML}`);
+});
+
+//select player game2
+const player1div = document.getElementById('player1');
+const player2div = document.getElementById('player2');
+
+player1div.addEventListener('click', function(){
+  player = 1;
+  player1div.style.backgroundColor = 'orange';
+})
+player2div.addEventListener('click', function(){
+  player = 2;
+  player1div.style.backgroundColor = 'orange';
+})
